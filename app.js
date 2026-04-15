ff613856f5fc45b68bcaf1fa2e6befa2b23863f4
Destiani09 <destiani.mahaputri@mhs.itenas.ac.id>
2026-04-16 03:44:27 +0700

    Fiksasi app.js Final

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const upload = multer({
     storage: multerS3({
         s3: s3,
         bucket: 'laporsampah-uts-bucket', 
-        acl: 'public-read',
+        //acl: 'public-read',
         metadata: function (req, file, cb) {
             cb(null, { fieldName: file.fieldname });
         },
